2c79e239a1a6750f3822df454e85027d8c8c357e
Viggo Casciano <viggo.casciano@ensto.com>
2024-09-13 13:16:12 +0200

    solve issue with build and followgames

--- src/commands/followgames.rs
+++ src/commands/followgames.rs
@@ -1,13 +1,16 @@
-use poise::Modal;
-use reqwest::Client;
-use std::collections::HashMap;
+// use poise::Modal;
+// use reqwest::Client;
+// use std::collections::HashMap;
+use crate::models::{Error, Data};
 
 /// This command will allow users to follow live games.
 /// Functionality will be implemented soon.
 #[poise::command(slash_command)]
-pub async fn followgames(ctx: Context<'_>) -> Result<(), Error> {
-    // Inform the user that this feature is not available yet
-    ctx.say("This feature will be added soon. Stay tuned!").await?;
+pub async fn followgames(
+    ctx: poise::ApplicationContext<'_, Data, Error>
+    ) -> Result<(), Error> {
+        // Inform the user that this feature is not available yet
+        ctx.say("This feature will be added soon. Stay tuned!").await?;
 
-    Ok(())
-}
+        Result::Ok(())
+    }
