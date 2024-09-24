9a0744962b21d3e90868f33719a3f1d247be4c6b
shvvkz <shvvkz@users.noreply.github.com>
2024-09-24 13:55:39 +0000

    deploy: c300cc261cd01fd72c216e12a3c9ca636a90192b

--- stat_summoner/utils/sidebar-items.js
+++ stat_summoner/utils/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["determine_solo_flex","get_emoji","is_valid_game_mode","region_to_string","seconds_to_time","time_since_game_ended"]};
+window.SIDEBAR_ITEMS = {"fn":["determine_solo_flex","get_emoji","get_game_mode","is_valid_game_mode","region_to_string","seconds_to_time","time_since_game_ended"]};
