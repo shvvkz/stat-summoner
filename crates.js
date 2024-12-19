95ac3d7bf3cfbcc753575e1c529e75ba82aa782b
shvvkz <shvvkz@users.noreply.github.com>
2024-12-19 10:22:53 +0000

    deploy: 7149e6b8ff53ab20d31649db594c5d7cba19eb02

--- crates.js
+++ crates.js
@@ -1,1 +1,2 @@
 window.ALL_CRATES = ["stat_summoner"];
+//{"start":21,"fragment_lengths":[15]}
--- trait.impl/poise/modal/trait.Modal.js
+++ trait.impl/poise/modal/trait.Modal.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"stat_summoner":[["impl Modal for <a class=\"struct\" href=\"stat_summoner/models/modal/struct.ChampionsInfosModal.html\" title=\"struct stat_summoner::models::modal::ChampionsInfosModal\">ChampionsInfosModal</a>"],["impl Modal for <a class=\"struct\" href=\"stat_summoner/models/modal/struct.FollowGamesModal.html\" title=\"struct stat_summoner::models::modal::FollowGamesModal\">FollowGamesModal</a>"],["impl Modal for <a class=\"struct\" href=\"stat_summoner/models/modal/struct.LolStatsModal.html\" title=\"struct stat_summoner::models::modal::LolStatsModal\">LolStatsModal</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["stat_summoner",[["impl Modal for <a class=\"struct\" href=\"stat_summoner/models/modal/struct.ChampionsInfosModal.html\" title=\"struct stat_summoner::models::modal::ChampionsInfosModal\">ChampionsInfosModal</a>"],["impl Modal for <a class=\"struct\" href=\"stat_summoner/models/modal/struct.FollowGamesModal.html\" title=\"struct stat_summoner::models::modal::FollowGamesModal\">FollowGamesModal</a>"],["impl Modal for <a class=\"struct\" href=\"stat_summoner/models/modal/struct.LolStatsModal.html\" title=\"struct stat_summoner::models::modal::LolStatsModal\">LolStatsModal</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[586]}
