56b427ebe42f25a91682b49bedb78f531feb5d5b
shvvkz <shvvkz@users.noreply.github.com>
2025-09-05 11:06:35 +0000

    deploy: 8ddd016b64ed460b8c67fa8bb3d38bdd5ac0a45e

---
+++ trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["stat_summoner",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"stat_summoner/module/loop_module/utils/struct.ChampionInfo.html\" title=\"struct stat_summoner::module::loop_module::utils::ChampionInfo\">ChampionInfo</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[359]}
--- trait.impl/core/str/traits/trait.FromStr.js
+++ trait.impl/core/str/traits/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["stat_summoner",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"stat_summoner/module/askingforflex/interaction_flex_buttons/enum.Role.html\" title=\"enum stat_summoner::module::askingforflex::interaction_flex_buttons::Role\">Role</a>"]]]]);
+    var implementors = Object.fromEntries([["stat_summoner",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"stat_summoner/module/askingforflex/interaction_flex_buttons/enum.Role.html\" title=\"enum stat_summoner::module::askingforflex::interaction_flex_buttons::Role\">Role</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
